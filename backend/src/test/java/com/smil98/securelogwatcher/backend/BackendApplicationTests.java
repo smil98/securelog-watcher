06a44fb571af1c0db705a29cde0cd88855e6a942
smil98 <seungmilee98@gmail.com>
2025-06-06 15:09:35 +0900

    Backend Settings - Gradle

---
+++ backend/src/test/java/com/smil98/securelogwatcher/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smil98.securelogwatcher.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
